812f749988a4e94ad463854de670defead119438
Konemiees <konsta.sinisalo@gmail.com>
2015-09-19 21:30:43 +0300

    Simple Main menu

---
+++ Assets/Scripts/MainMenuHandle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuHandle : MonoBehaviour {
+
+	public GameObject mainMenuCanvas;
+	public GameObject instructionCanvas;
+
+	void Start(){
+		mainMenuCanvas.SetActive (true);
+		instructionCanvas.SetActive (false);
+	}
+
+	public void PlayGame(string scene){
+		Application.LoadLevel (scene);
+	}
+
+	public void EndGame(){
+		Application.Quit();
+	}
+
+	public void Instructions(){
+		mainMenuCanvas.SetActive (false);
+		instructionCanvas.SetActive (true);
+	}
+
+	public void BackButton(){
+		mainMenuCanvas.SetActive (true);
+		instructionCanvas.SetActive (false);
+	}
+
+}
